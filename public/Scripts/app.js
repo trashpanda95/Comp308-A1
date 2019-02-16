27d196eec32edb99dff00dbccb1174c76dd39044
trashpanda95 <aubrian230@gmail.com>
2019-02-16 04:02:17 -0500

    Add files via upload

---
+++ public/Scripts/app.js
@@ -0,0 +1,8 @@
+// IIFE -- Imediately Invoked Function Expression
+(function(){
+    function Start(){
+        console.log(`%c App Started...`,
+        "font-size: 20px; color: blue; font-weight: bold");
+    }
+    window.addEventListener("load", Start);
+})();
